73a587a11a3ee1c1b1809618eee6ff40ab8405ad
mertcansucu <mrtcnscc@gmail.com>
2023-11-29 20:35:33 +0300

    product list, get form,categories select, product view model

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -1,21 +1,47 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController()
     {
-        _logger = logger;
+         
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string searchString,string category)
     {
-        return View();
+        //search 
+        var products = Repository.Products; 
+
+        if(!String.IsNullOrEmpty(searchString)){//arama yaptım
+            ViewBag.searchString = searchString;//aranan kelime kalsın diye
+            products = products.Where(p => p.Name.ToLowerInvariant().Contains(searchString)).ToList();
+        }
+
+        if(!String.IsNullOrEmpty(category) && category != "0"){
+            //category != "0" filtrede hepsi seçersem tüm liste gelsin diye
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+        }
+
+        /*1. yolkategori bilgileri sayfa üzerine taşıdım Filtreleme
+        //kategori bilgileri sayfa üzerine taşıdım Filtreleme
+        ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name",category);//category de ekledim çünkü ben telefonu seçip filtreleme yaptığımda o orda yazılı kalsın diye
+        */
+        
+
+        //2.yol kategori bilgileri sayfa üzerine taşıdım Filtreleme
+        
+        var model = new ProductViewModel{
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
+        return View(model);
     }
 
     public IActionResult Privacy()
@@ -23,9 +49,5 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,20 @@ namespace FormsApp.Models
 {
     public class Product
     {
-        
+        [Display(Name ="Urun Id")]//display ile productid yerine bu görünecek
+        public int ProductId { get; set; }
+
+        [Display(Name ="Urun Adı")]
+        public string? Name { get; set; }
+
+         [Display(Name ="Fiyat")]
+        public decimal Price { get; set; }
+
+         [Display(Name ="Resim")]
+        public string Image { get; set; } = string.Empty;//? yerine bu şekilde de yapabilirim
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
     }
 }
+//1 iphone 14 1
+//2 iphone 15 1
