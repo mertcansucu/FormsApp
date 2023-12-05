af75cf0032d07c7a527e7bb08f3de66195fb9f03
mertcansucu <mrtcnscc@gmail.com>
2023-12-05 05:02:25 +0300

    Registering the Update,Product Deletion,Deleting a Product via Post Form and Batch Update

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -57,14 +57,13 @@ public class HomeController : Controller
     //public IActionResult Create([Bind("Name","Price")]Product model)
     public async Task<IActionResult> Create(Product model, IFormFile imageFile)//IFormFile imageFile içerisine eklenen img doğru olup olmadığını kontrol eder
     {
-        var allowedExtensions = new[] {".jpg",".jpeg",".png"};//kabul edilecek uzantıları ekledim
-        var extension = Path.GetExtension(imageFile.FileName);//dosyanın uzantısını alır ; mesela burda imageFile.FileName buna bakar abc.jpg ise "jpg" kısmını alır
-        var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");//burda random isim oluşturup(Guid.NewGuid()) üste dosyadan aldığım uzatıyı ekliyorum direk
-
-        var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img", randomFileName);//Yüklenen resmi path.combine methodu içinde Directory.GetCurrentDirectory() ile şuan bulunan bu alnadaki dizininin içinde wwwroot/img içine imageFile.FileName ordaki remin adı ile kayıt et diyorum, ancak upload ettiğim klasörede aynı isimde başka bir klasör varsa ; random olarak isim verilebilir direk ya da if döngüsü ile isim kontrol edilir varsa sonuna başka bir şey eklenebilir. Burda da ben rastgele isim oluşturup direk ekledim onun içinde imageFile.FileName yerine randomFileName olarak kaydettim
-
+        var extension = "";//alt tarafta da kullandığım için başta tanımladım
         //yüklenen remin istediğim uzantıda oluğ olamadığını kontrol ediyorum
         if(imageFile != null){//kontrol etmeden resim dosyası seçilip seçilmediğini kontrol ediyorum
+
+            var allowedExtensions = new[] {".jpg",".jpeg",".png"};//kabul edilecek uzantıları ekledim
+            extension = Path.GetExtension(imageFile.FileName);//dosyanın uzantısını alır ; mesela burda imageFile.FileName buna bakar abc.jpg ise "jpg" kısmını alır
+
             if(!allowedExtensions.Contains(extension)){//kontrol işlemi yapıyorum istediğim uzatıda değilse hata mesajı verdim
                 ModelState.AddModelError("","Geçerli bir resim seçiniz");
             }
@@ -73,21 +72,24 @@ public class HomeController : Controller
         if(ModelState.IsValid){//forma giren değerler validation hatası içermiyorsa çalışsın diyorum
 
             if(imageFile != null){//resim dosyası boş değilse 
+                var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");//burda random isim oluşturup(Guid.NewGuid()) üste dosyadan aldığım uzatıyı ekliyorum direk
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img", randomFileName);//Yüklenen resmi path.combine methodu içinde Directory.GetCurrentDirectory() ile şuan bulunan bu alnadaki dizininin içinde wwwroot/img içine imageFile.FileName ordaki remin adı ile kayıt et diyorum, ancak upload ettiğim klasörede aynı isimde başka bir klasör varsa ; random olarak isim verilebilir direk ya da if döngüsü ile isim kontrol edilir varsa sonuna başka bir şey eklenebilir. Burda da ben rastgele isim oluşturup direk ekledim onun içinde imageFile.FileName yerine randomFileName olarak kaydettim
 
                 //resim eklenmesinde sorun olmazsa burda stremi oluşturuyorum çünkü kapsamdan çıktığında bellekten silinsin diye
                 using(var stream = new FileStream(path, FileMode.Create)){
                     await imageFile.CopyToAsync(stream);//ilgili dizine kopyaladım resmi ve çalışması için Task<IActionResult> yaptım
                 }
+                //resim varsa oluşacak
+                model.Image = randomFileName;//yeni eklediğimiz resim gözükebilsin diye eklememiz lazım
+
+                model.ProductId = Repository.Products.Count + 1;
+
+                Repository.CreateProduct(model);
+                // return View();
+                return RedirectToAction("Index");//yeni ürün kayıtından sonra index sayfasına git dedim
             }
 
-            
-            model.Image = randomFileName;//yeni eklediğimiz resim gözükebilsin diye eklememiz lazım
-
-            model.ProductId = Repository.Products.Count + 1;
-
-            Repository.CreateProduct(model);
-            // return View();
-            return RedirectToAction("Index");//yeni ürün kayıtından sonra index sayfasına git dedim
         }
         
         ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name");
@@ -96,6 +98,7 @@ public class HomeController : Controller
         
     }
 
+    [HttpGet]
     public IActionResult Edit(int? id){
         if(id == null){
             return NotFound();
@@ -107,6 +110,84 @@ public class HomeController : Controller
         }
         ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name");
         return View(entity);//kayıtı sayfaya taşıdım
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Product model, IFormFile? imageFile){//IFormFile? resim seçilme zorunluluğunu kaldırdım
+        if(id != model.ProductId){
+            return NotFound();
+        }
+
+        if(ModelState.IsValid){
+            if(imageFile != null){//resim dosyası boş değilse 
+                var extension = Path.GetExtension(imageFile.FileName);//dosyanın uzantısını alır ; mesela burda imageFile.FileName buna bakar abc.jpg ise "jpg" kısmını alır
+                var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");//burda random isim oluşturup(Guid.NewGuid()) üste dosyadan aldığım uzatıyı ekliyorum direk
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img", randomFileName);//Yüklenen resmi path.combine methodu içinde Directory.GetCurrentDirectory() ile şuan bulunan bu alnadaki dizininin içinde wwwroot/img içine imageFile.FileName ordaki remin adı ile kayıt et diyorum, ancak upload ettiğim klasörede aynı isimde başka bir klasör varsa ; random olarak isim verilebilir direk ya da if döngüsü ile isim kontrol edilir varsa sonuna başka bir şey eklenebilir. Burda da ben rastgele isim oluşturup direk ekledim onun içinde imageFile.FileName yerine randomFileName olarak kaydettim
+
+                //resim eklenmesinde sorun olmazsa burda stremi oluşturuyorum çünkü kapsamdan çıktığında bellekten silinsin diye
+                using(var stream = new FileStream(path, FileMode.Create)){
+                    await imageFile.CopyToAsync(stream);//ilgili dizine kopyaladım resmi ve çalışması için Task<IActionResult> yaptım
+                }
+                model.Image = randomFileName;
+            }
+            Repository.EditProduct(model);
+            return RedirectToAction("Index");
+        }
+        //else durumu düşün burayı eğer hata olursa sayfayı yeniden yükledim
+        ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId","Name");
+        return View(model);
+    }
+
+    //silme işleminde kişi eğer ürünü silme istiyorsa delete butonuna basıp get ile bilgileri DeleteConfirm göderip orda tekrar silmek istiyor musun diye soruyorum oda evet derse post ile bu işlemi onaylamak veriyi siliyorum
+    [HttpGet]//get ile deleted işlemi
+    public IActionResult Delete(int? id)
+    {
+        if(id == null)
+        {
+            return NotFound();        
+        }
+
+        var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+        if(entity == null)
+        {
+            return NotFound();
+        }
+
+
+        //burayı kapatma nedenim direk silme yapıyorum Get delete, onun yerine silmek istiyor musun gibi soru kullanmak
+        // Repository.DeleteProduct(entity);
+        // return RedirectToAction("Index");//silindikten sonra listeyi getir
+
+        return View("DeleteConfirm", entity);
+    }
+
+    [HttpPost]//post deleted
+    public IActionResult Delete(int id, int ProductId)
+    {
+        if(id != ProductId)
+        {
+            return NotFound();
+        }
+
+        var entity = Repository.Products.FirstOrDefault(p => p.ProductId == ProductId);
+        if(entity == null)
+        {
+            return NotFound();
+        }
+
+        Repository.DeleteProduct(entity);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]//toplu edit yani güncelleme için yaptım modernbinding işlemi
+    public IActionResult EditProducts(List<Product> Products)
+    {
+        foreach (var product in Products)
+        {
+            Repository.EditIsActive(product);
+        }
+        return RedirectToAction("Index");
     }
     
 }
--- FormsApp/Models/Repository.cs
+++ FormsApp/Models/Repository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace FormsApp.Models
 {
-    public class Repository
+    public class Repository //benim veritabanım
     {
         private static readonly List<Product> _products = new();
         private static readonly List<Category> _categories = new();
@@ -14,11 +14,11 @@ namespace FormsApp.Models
             _categories.Add(new Category {CategoryId = 2, Name = "Bilgisayar"});
 
             _products.Add(new Product {ProductId = 1, Name = "Iphone 14", Price = 40000, IsActive = true, Image = "1.jpg", CategoryId = 1});
-            _products.Add(new Product {ProductId = 2, Name = "Iphone 15", Price = 50000, IsActive = true, Image = "2.jpg", CategoryId = 1});
+            _products.Add(new Product {ProductId = 2, Name = "Iphone 15", Price = 50000, IsActive = false, Image = "2.jpg", CategoryId = 1});
             _products.Add(new Product {ProductId = 3, Name = "Iphone 16", Price = 60000, IsActive = true, Image = "3.jpg", CategoryId = 1});
-            _products.Add(new Product {ProductId = 4, Name = "Iphone 17", Price = 70000, IsActive = true, Image = "4.jpg", CategoryId = 1});
+            _products.Add(new Product {ProductId = 4, Name = "Iphone 17", Price = 70000, IsActive = false, Image = "4.jpg", CategoryId = 1});
 
-            _products.Add(new Product {ProductId = 5, Name = "Macbook Air", Price = 80000, IsActive = true, Image = "5.jpg", CategoryId = 2});
+            _products.Add(new Product {ProductId = 5, Name = "Macbook Air", Price = 80000, IsActive = false, Image = "5.jpg", CategoryId = 2});
             _products.Add(new Product {ProductId = 6, Name = "Macbook Pro", Price = 90000, IsActive = true, Image = "6.jpg", CategoryId = 2});
 
         }
@@ -31,6 +31,41 @@ namespace FormsApp.Models
         //Create.cshtlm de oluşturduğum yeni kayıt eklemek için ekledim
         public static void CreateProduct(Product entity){
             _products.Add(entity);
+        }
+
+        //Editle kayıtları güncellemek için ekledim
+        public static void EditProduct(Product updateProduct){
+            var entity = _products.FirstOrDefault(p => p.ProductId == updateProduct.ProductId);
+
+            if(entity != null){
+                //bunu böyle yapmamın nedeni EditIsActive diye yeni method yerine hepsini bu şekilde yaparak yeni bir değer girişi var o zaman güncelle derim eğer yeni değer girilmemişse de önceki değeri gir derim
+                if(!string.IsNullOrEmpty(updateProduct.Name)){
+                    entity.Name = updateProduct.Name;
+                }
+                entity.Price = updateProduct.Price;
+                entity.Image = updateProduct.Image;
+                entity.CategoryId = updateProduct.CategoryId;
+                entity.IsActive = updateProduct.IsActive;
+            }
+        }
+
+        //checkbox ile toplu güncelleme için
+        //burda yeni method tanımlamamın nedeni toplu güncellemede checkboxa göre toplu güncelliyor ama üsteki methodu çağırarak yaparsam diğer alanları güncellemediğim için o alanları boş getiriyor bunu engellemek için yeni method tanımlayıp sadece bunu güncelle yaptım ya da onun yerine üsteki methodu if dögüsü ile kontrol ederek yapmam lazım
+        public static void EditIsActive(Product updateProduct){
+            var entity = _products.FirstOrDefault(p => p.ProductId == updateProduct.ProductId);
+
+            if(entity != null){
+                entity.IsActive = updateProduct.IsActive;
+            }
+        }
+
+        //Delete silme işlemi için ekledim
+        public static void DeleteProduct(Product deletedProduct){
+            var entity = _products.FirstOrDefault(p => p.ProductId == deletedProduct.ProductId);
+
+            if(entity != null){
+                _products.Remove(entity);
+            }
         }
         public static List<Category> Categories{
             get{
